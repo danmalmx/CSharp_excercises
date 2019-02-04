6878599677cae62f34e712c19baddcedeb4c5bac
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2019-02-04 14:16:46 +0100

    adding excercises projects

---
+++ CSharp_excercises/CSharp_excercises/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_excercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Excercises 11
+            //Console.Write("Enter your age: ");
+            //var age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("You look older than {0}", age);
+            //Console.ReadKey();       
+            
+            //Excercises 12
+            Console.Write("Enter your age: ");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You look older than {0}", age);
+            Console.ReadKey();
+
+        }
+    }
+}
