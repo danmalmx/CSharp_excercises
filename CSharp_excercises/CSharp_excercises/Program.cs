e1592087f91e161e74710b8392bdb44c8895857c
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2019-02-04 16:09:07 +0100

    adding excercise 16

--- CSharp_excercises/CSharp_excercises/Program.cs
+++ CSharp_excercises/CSharp_excercises/Program.cs
@@ -10,13 +10,15 @@ namespace CSharp_excercises
     {
         static void Main(string[] args)
         {
-            //Excercises 11
+            // Excercises (Basic) from https://www.w3resource.com/csharp-exercises/basic/index.php
+
+            //Excercise 11
             //Console.Write("Enter your age: ");
             //var age = Convert.ToInt32(Console.ReadLine());
             //Console.WriteLine("You look older than {0}", age);
             //Console.ReadKey();       
 
-            ////Excercises 12
+            ////Excercise 12
             //Console.Write("Enter a number: ");
             //var number = Console.ReadLine();
             //Console.WriteLine("{0} {0} {0} {0}", number);
@@ -26,7 +28,19 @@ namespace CSharp_excercises
 
             //Console.ReadKey();
 
-            ////Excercises 13
+            ////Excercise 13
+            //Console.Write("Enter a number: ");
+            //var number = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("{0}{0}{0}", number);
+            //Console.WriteLine("{0} {0}", number);
+            //Console.WriteLine("{0} {0}", number);
+            //Console.WriteLine("{0} {0}", number);
+            //Console.WriteLine("{0}{0}{0}", number);
+
+            //Console.ReadKey();
+
+            ////Excercises 14
             //Console.Write("Enter degrees in Celcius: ");
             //var number = Convert.ToInt32(Console.ReadLine());
             //float kelvin = number +273.15f;
@@ -36,17 +50,29 @@ namespace CSharp_excercises
             //Console.WriteLine("Farenheit = {0}", farenheit);
             //Console.ReadKey();
 
-            //Excercises 14
-            Console.Write("Text: ");
-            var text = Console.ReadLine();
+            //Excercises 15
+            //Console.Write("Text: ");
+            //var text = Console.ReadLine();
 
-            Console.WriteLine("{0}", text.Remove(1, 1));
-            Console.WriteLine("{0}", text.Remove(text.Length - 1, 1));
-            Console.WriteLine("{0}", text.Remove(0, 1));
+            //Console.WriteLine("{0}", text.Remove(1, 1));
+            //Console.WriteLine("{0}", text.Remove(text.Length - 1, 1));
+            //Console.WriteLine("{0}", text.Remove(0, 1));
 
+            //Console.ReadKey();
+
+            //Excercises 16
+
+            Console.WriteLine(first_last("w3resource"));
+            Console.WriteLine(first_last("Python"));
+            Console.WriteLine(first_last("x"));
 
             Console.ReadKey();
         }
 
+        public static string first_last(string ustr)
+        {
+            return ustr.Length > 1
+                ? ustr.Substring(ustr.Length - 1) + ustr.Substring(1, ustr.Length - 2) + ustr.Substring(0, 1) : ustr;
+        }
     }
 }
