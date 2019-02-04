60f08a34ec5b89aa48bdc284790c18fc3825803d
danmalmx <danmalmx@gmail.com>
2019-02-04 19:43:18 +0100

    adding excercise 18

--- CSharp_excercises/CSharp_excercises/Program.cs
+++ CSharp_excercises/CSharp_excercises/Program.cs
@@ -12,6 +12,95 @@ namespace CSharp_excercises
         {
             // Excercises (Basic) from https://www.w3resource.com/csharp-exercises/basic/index.php
 
+            ////Excercise 1
+            //Console.WriteLine("Hello:");
+            //Console.WriteLine("Daniel!");
+
+            ////Excercise 2
+            //Console.WriteLine(1+2);
+
+            ////Excercise 3
+            //Console.WriteLine(10/5);
+
+            ////Excercise 4
+            //Console.WriteLine(-1 + 4 * 6);
+            //Console.WriteLine((35 + 5) % 7);
+            //Console.WriteLine(14 +- 4 * 6 /11 );
+            //Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+
+            ////Excercise 5
+            //Console.WriteLine("Number 1:");
+            //var n1 = Console.ReadLine();
+            //int first = Convert.ToInt32(n1);
+
+            //Console.WriteLine("Number 2:");
+            //var n2 = Console.ReadLine();
+            //int second = Convert.ToInt32(n2);
+
+            //var temp = n1;
+            //n1 = n2;
+            //n2 = temp;
+
+            //Console.WriteLine(); 
+            //Console.WriteLine("Swap magic");
+            //Console.WriteLine();
+
+            //Console.WriteLine("First number: {0}. Second number: {1}", n1, n2);
+
+            ////Excercise 6
+            //Console.WriteLine("Number 1:");
+            //var n1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Number 2:");
+            //var n2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Number 3:");
+            //var n3 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Output of {0} * {1} * {2} = {3}", n1, n2, n3, n1 * n2 * n3);
+
+            ////Excercise 7
+            //Console.WriteLine("Number 1:");
+            //var n1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Number 2:");
+            //var n2 = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.WriteLine("{0} + {1} = {2}", n1, n2, n1 + n2);
+            //Console.WriteLine("{0} - {1} = {2}", n1, n2, n1 - n2);
+            //Console.WriteLine("{0} * {1} = {2}", n1, n2, n1 * n2);
+            //Console.WriteLine("{0} / {1} = {2}", n1, n2, n1 / n2);
+            //Console.WriteLine("{0} % {1} = {2}", n1, n2, n1 % n2);
+
+            ////Excercise 8
+            //Console.WriteLine("Number 1:");
+            //var n1 = Convert.ToInt32(Console.ReadLine());
+
+            //for (var i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine("{0} * {1} = {2}", n1, i, n1 * i);
+            //}
+
+            ////Excercise 9
+            //Console.WriteLine("Number 1:");
+            //var n1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Number 2:");
+            //var n2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Number 3:");
+            //var n3 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Number 4:");
+            //var n4 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("The average of {0}, {1}, {2}, {3} is {4}", n1, n2, n3, n4, (n1 + n2 + n3 + n4)/4);
+
+            //Excercise 10
+            //Console.WriteLine("Number 1:");
+            //var x = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Number 2:");
+            //var y = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Number 3:");
+            //var z = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine(@"Result of {0}, {1} and {2}, (x+y).z is {3} and x.y + y.z is {4}", -x, -y, -z, (x + y) * z, (x * y) + (y * z));
+
             //Excercise 11
             //Console.Write("Enter your age: ");
             //var age = Convert.ToInt32(Console.ReadLine());
@@ -76,11 +165,27 @@ namespace CSharp_excercises
             //}
 
             //Excercises 17
-            Console.Write("Text: ");
-            string text = Console.ReadLine();
-            string extract = text.Substring(0, 1);
-            Console.WriteLine("{0}{1}{0}", extract, text, extract);
-            Console.ReadKey();
+            //Console.Write("Text: ");
+            //string text = Console.ReadLine();
+            //string extract = text.Substring(0, 1);
+            //Console.WriteLine("{0}{1}{0}", extract, text, extract);
+            //Console.ReadKey();
+
+            //Excercise 18
+            Console.Write("Enter first integer: ");
+            var number_1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second integer: ");
+            var number_2 = Convert.ToInt32(Console.ReadLine());
+            if (number_1 > 0 || number_2 > 0)
+            {
+                Console.WriteLine("Check if one is negative and one is positive: {0}", false);
+                Console.ReadKey();
+            }
+            else 
+            {
+                Console.WriteLine("Check if one is negative and one is positive: {0}", true);
+                Console.ReadKey();
+            }
         }
     }
 }
